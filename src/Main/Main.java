3419634c53d3a425743e275d32e26d722607cc5a
Alejandro Garcia <alejandro.garciab@campusfp.es>
2023-03-21 11:52:53 +0100

    evidencia

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -4,15 +4,15 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//pepe
+		
 		CuentaBancaria cb = new CuentaBancaria();
 		try {
-			//cb.agregarSaldo(300);
-			//cb.retirarSaldo(150);
-			//cb.obtenerSaldo()//debe retornar 150sda
-			//cb.modificarCredito(1000);
-			//cb.solicitarPrestamo(1000);
-			//cb.obtenerPrestamo();//debe retornar 450
+			cb.agregarSaldo(3000);
+			cb.agregarSaldo(500);
+			cb.retirarSaldo(1000);
+			cb.agregarSaldo(800);
+			cb.solicitarPrestamo(10000);
+			cb.obtenerPrestamo();
 		} catch (Exception e) {
 		}
 	}
